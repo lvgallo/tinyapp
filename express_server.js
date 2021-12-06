e8fb06f85a89b531217758e81a3b32ea79898f1e
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2021-12-06 23:39:41 +0000

    adding a second route, page shows the new link, short and long URL, based on urls/:shortURL

--- express_server.js
+++ express_server.js
@@ -11,11 +11,21 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+app.get("/urls", (req, res) => { 
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/:shortURL", (req, res) => {
+  // const urlDatabase = {
+  //   "b2xVn2": "http://www.lighthouselabs.ca",
+  //   "9sm5xK": "http://www.google.com"}
+  const templateVars = { shortURL: req.params.shortURL, longURL:
+  urlDatabase.shortURL };
+  res.render("urls_show", templateVars);
+});
 app.get("/urls.json", (req, res) => { 
   res.json(urlDatabase); // print the object urlDatabase
 });
-
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n"); //World - bold as html 
 });
